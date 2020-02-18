a3412f295855fd45d70e2c389dcc2ea2089bc2bf
Craig Johnston <cjimti@gmail.com>
2020-02-17 23:04:17 -0800

    clear headers

--- cmd/jwtpxy.go
+++ cmd/jwtpxy.go
@@ -102,6 +102,7 @@ func (p *Proxy) handle(w http.ResponseWriter, r *http.Request) {
 
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 		// @TODO map claims to headers from config
+		r.Header = http.Header{}
 		r.Header.Add("From", claims["preferred_username"].(string))
 	} else {
 		w.WriteHeader(http.StatusUnauthorized)
