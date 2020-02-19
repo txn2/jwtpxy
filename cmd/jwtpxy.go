1bff2196228f1b4b28ba6bc826ad7b2e36812e01
Craig Johnston <cjimti@gmail.com>
2020-02-18 17:44:57 -0800

    Remove and re-add From header

--- cmd/jwtpxy.go
+++ cmd/jwtpxy.go
@@ -103,7 +103,7 @@ func (p *Proxy) handle(w http.ResponseWriter, r *http.Request) {
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 		// @TODO map claims to headers from config
 		// @TODO config headers to preserve
-		r.Header = http.Header{}
+		r.Header.Del("From")
 		r.Header.Add("From", claims["preferred_username"].(string))
 	} else {
 		w.WriteHeader(http.StatusUnauthorized)
